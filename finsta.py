e4796f30a07e639a9340ffea1aa18b1b51436f84
aojiu <xieweiyao@gmail.com>
2019-04-03 15:10:59 -0400

    merge with james and updata the error when there is no close friend group

--- finsta.py
+++ finsta.py
@@ -109,35 +109,45 @@ def post():
     cursor = conn.cursor();
     filepath = request.form["filepath"]
     allFollowers = request.form['allFollowers']
-    # for elem in request.form:
-    #     print("kaka")
-    # isPrivate = int(isPrivate)
-    # if isPrivate == 0:
-    #     print("is equal to 0")
-    # print(type(allFollowers))
     caption = request.form["caption"]
     timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
-    query = 'INSERT INTO photo VALUES(Null, %s, %s, %s, %s, %s)'
-    cursor.execute(query, (username, timestamp, filepath, caption, int(allFollowers)))
-    conn.commit()
+    error = None
+
 
     if allFollowers == "0":
-        query = 'SELECT DISTINCT photoID FROM Photo AS p WHERE p.allFollowers = %s AND p.photoID NOT IN (SELECT photoID from Photo NATURAL JOIN Share WHERE Photo.allFollowers = %s);'
-        cursor.execute(query, (0, 0))
-        data = cursor.fetchall()
-        print(data)
-        photoId = data[0]["photoID"]
-        # print(photoId)
-        conn.commit()
         query = 'SELECT groupName FROM CloseFriendGroup WHERE groupOwner = %s'
         cursor.execute(query, (username))
+        conn.commit()
         data = cursor.fetchall()
         print(data)
-        print(data[0]["groupName"])
-        print("abc")
-        conn.commit()
-        cursor.close()
-        return render_template("sharewith.html", closeFriendsGroup = data, photoId = photoId)
+        # print(data[0]["groupName"])
+
+        if data: #check if the user has at least one closefriend group so he can insert private photo
+            #insert the photo
+            query = 'INSERT INTO photo VALUES(Null, %s, %s, %s, %s, %s)'  # insert the photo into DB
+            cursor.execute(query, (username, timestamp, filepath, caption, int(allFollowers)))
+            conn.commit()
+            #select the photo id just inserted into DB which is not shared
+            query = 'SELECT DISTINCT photoID FROM Photo AS p WHERE p.allFollowers = %s AND p.photoID NOT IN (SELECT photoID from Photo NATURAL JOIN Share WHERE Photo.allFollowers = %s);'
+            cursor.execute(query, (0, 0))
+            data = cursor.fetchall()
+            print(data)
+            photoId = data[0]["photoID"]
+            # print(photoId)
+            conn.commit()
+
+            # task xiewy: need to add error here no closefriendgroup
+            print("abc")
+
+            cursor.close()
+            return render_template("sharewith.html", closeFriendsGroup = data, photoId = photoId)
+        else:
+            # returns an error message to the html page
+            session["username"] = username
+            print("you get into the else no data condition")
+            error = 'should have at least one close friends to post a private photo'
+            # return redirect(url_for('home',error = error))
+            return render_template("home.html", username = username, error = error)
     cursor.close()
     return redirect(url_for('home'))
 
@@ -171,15 +181,30 @@ def share():
 #     cursor.close()
 #     return render_template('select_blogger.html', user_list=data)
 #
-# @app.route('/show_posts', methods=["GET", "POST"])
-# def show_posts():
-#     poster = request.args['poster']
-#     cursor = conn.cursor();
-#     query = 'SELECT ts, blog_post FROM blog WHERE username = %s ORDER BY ts DESC'
-#     cursor.execute(query, poster)
-#     data = cursor.fetchall()
-#     cursor.close()
-#     return render_template('show_posts.html', poster_name=poster, posts=data)
+@app.route('/show_photos', methods=["GET", "POST"])
+def show_photos():
+     user_name = session["username"]
+     cursor = conn.cursor()
+     query1 = "SELECT photoId, photoOwner, caption, filepath, timestamp FROM photo JOIN person ON photo.photoOwner = person.username WHERE isPrivate = 0 OR username = %s OR username IN (SELECT followeeUsername FROM follow WHERE followerUsername = %s and acceptedfollow = 1) AND allFollowers = 1 ORDER BY `photo`.`timestamp` DESC"
+     cursor.execute(query1, (user_name, user_name))
+     data1 = cursor.fetchall()
+     for i in range(len(data1)):
+         data1[i]["fname"] = ""
+         data1[i]["lname"] = ""
+     query2 = "SELECT photoId, fname, lname FROM person JOIN (SELECT username, photoId, photoOwner, caption, filepath, timestamp FROM photo NATURAL JOIN tag WHERE acceptedTag = 1) t2 ON person.username = t2.username"
+     cursor.execute(query2)
+     data2 = cursor.fetchall()
+     for i in range(len(data1)):
+         for j in range(len(data2)):
+             if data1[i]["photoId"] == data2[j]["photoId"]:
+                 if data1[i]["fname"] == "":
+                     data1[i]["fname"] = data2[j]["fname"]
+                     data1[i]["lname"] = data2[j]["lname"]
+                 else:
+                    data1[i]["fname"] += ", " + data2[j]["fname"]
+                    data1[i]["lname"] += ", " + data2[j]["lname"]
+     cursor.close()
+     return render_template('show_photos.html', user_name=user_name, photos=data1)
 
 @app.route('/logout')
 def logout():
@@ -190,8 +215,5 @@ app.secret_key = 'some key that you will never guess'
 #Run the app on localhost port 5000
 #debug = True -> you don't have to restart flask
 #for changes to go through, TURN OFF FOR PRODUCTION
-# if __name__ == "__main__":
-#     app.run('127.0.0.1', 5000, debug = True)
-
 if __name__ == "__main__":
     app.run()
